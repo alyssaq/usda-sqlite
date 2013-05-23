b95247bcf3220950d55208dd2ee758e515bf2b0b
czarandy <andy.goder@gmail.com>
2013-05-23 15:51:28 -0700

    Added another example script to get omega 3 fatty acid info for foods

---
+++ example/omega3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json
+import sqlite3
+import sys
+
+if len(sys.argv) != 2:
+  sys.exit('Must provide term to search for')
+
+conn = sqlite3.connect('../usda.sql3')
+c = conn.cursor()
+c.execute("""
+  SELECT
+    food.long_desc,
+    SUM(nutrition.amount)
+  FROM food
+  JOIN nutrition
+  ON nutrition.food_id = food.id
+  WHERE food.long_desc LIKE ?
+  AND nutrition.nutrient_id IN (621, 629)
+  GROUP BY food.long_desc
+""", ('%' + sys.argv[1] + '%',))
+
+for row in c:
+  print row[0], row[1]
--- example/search.py
+++ example/search.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 2:
   sys.exit('Must provide text to search for')
 
 conn = sqlite3.connect('../usda.sql3')
-search_clause = sys.argv[1] + '%'
+search_clause = '%' + sys.argv[1] + '%'
 c = conn.cursor()
 c.execute('SELECT id, long_desc FROM food WHERE long_desc LIKE ?', (search_clause,))
 for row in c:
